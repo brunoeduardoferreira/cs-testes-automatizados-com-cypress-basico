5a215d3f104d8b90925582b2d8a99f9a42cb4021
Bruno E. M . Ferreira <89544503+brunoeduardoferreira@users.noreply.github.com>
2021-10-17 15:15:01 -0300

    Aula - Digitando em campos de Texto

--- cypress-basic-course/cypress/integration/ticket.spec.js
+++ cypress-basic-course/cypress/integration/ticket.spec.js
@@ -2,7 +2,20 @@ describe("Tickets", () => {
   // Passa a URL que será chamada
   beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
 
+  // teste que irá preencher os campos de Texto da Aplicação
+  it.only("fills all the text input fields", () => {
+    const firstName = "Bruno";
+    const lastName = "Ferreira"
+
+    cy.get("#first-name").type(firstName);
+    cy.get("#last-name").type(lastName);
+    cy.get("#email").type("brunoemf@gmail.com");
+    cy.get("#requests").type("Gamer");
+    cy.get("#signature").type(`${firstName} ${lastName}`);
+  });
+
+
   it("has 'TICKETBOX' header's heading", () => {
 
-  })
+  });
 })
